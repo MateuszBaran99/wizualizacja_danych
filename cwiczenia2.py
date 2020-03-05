2ce49d63880d65652d163dc4b1bb12610a012685
Testuje gita <local@WMII-8VLIFGS0C7.wmii.local>
2020-03-05 09:16:43 +0100

    lol

---
+++ cwiczenia2.py
@@ -0,0 +1,17 @@
+wejscie=input("Wprowadz wartosc: ")
+print(wejscie)
+
+# liczba =10
+# if liczba < 10 and liczba > 0:
+#     print()
+
+# else:
+#     print("Poza zakresem")
+
+liczba =10
+if 10 > liczba > 0:
+    print()
+
+else:
+    print("Poza zakresem")
+
