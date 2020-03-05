9cd500a723cfee79f075a111db4f7b58f4343c75
Testuje gita <local@WMII-8VLIFGS0C7.wmii.local>
2020-03-05 09:46:04 +0100

    lol

--- cwiczenia2.py
+++ cwiczenia2.py
@@ -15,3 +15,31 @@ if 10 > liczba > 0:
 else:
     print("Poza zakresem")
 
+
+# rozne od liczba != 0
+# rowne liczba ==0
+# liczba is  not True
+# liczba is True
+
+if liczba in [1, 2, 3, 10]:        #sprawdza czy liczba jest w liscie
+    print("jest w liscie")
+else:
+    print("Nie ma na liscie")
+
+
+for liczba in [1, 2, 3, 10]:
+    print(liczba)
+
+imie = "Malgorzata"
+for litera in imie:
+    print(litera)
+
+for liczba in range(5): #start, stop, step
+    print(liczba)
+
+print(list(range(5)))
+# [0, 1, 2, 3, 4]
+print(list(range(10, 0, -2)))
+
+imie = "Malgorzata"
+print(imie[0:6:2])
