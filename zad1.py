9fcba4afac5f8d451e09165fbbae4e0b0c6e79ea
SCREAMIE <scgfxpl@onet.pl>
2020-03-05 10:57:04 +0100

    dddd

--- zad1.py
+++
@@ -1,7 +0,0 @@
-tekst = input("Zapodaj jakis tekst: ")
-X=0
-for i in tekst:
-    if i == " ":
-        X=X+1
-    
-print(X)
--- zad4.py
+++
@@ -1,7 +0,0 @@
-liczba = input("zapodaj liczbe: ")
-liczba = int(liczba)
-if liczba >= 0:
-    print(liczba)
-else:
-    liczba = -liczba
-    print(liczba)
